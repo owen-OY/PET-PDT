14a9dd7c4086f25e0705a6752eca4e050df568c2
anker kintan <88662668+ankerKintan@users.noreply.github.com>
2021-10-29 21:59:31 +0800

    Create code_批量删除小于3S的音频视频文件.py

---
+++ T7200/其它脚本/code_批量删除小于3S的音频视频文件.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+
+# @Author       : xuanye.wang
+# @Date         : 2021/10/29 22:14
+# @File         : code_批量删除小于3S的音频视频文件.py
+'''
+@Mark:          :1、该脚本只能删除音频视频文件；
+                 2、输入需要目录路径即可；
+'''
+
+import eyed3
+import time
+import os
+from loguru import logger
+
+for root, dirs, files in os.walk(f'./result'):  # 判断下载的音频时长，小于3S则删除该文件
+    for file in files:
+        musicPath = rf'./result/{file}'
+        time.sleep(0.5)
+        mp3Info = eyed3.load(musicPath).info.time_secs
+        if mp3Info < 3:
+            logger.info('正在删除文件--%s' % musicPath)
+            os.remove(musicPath)
