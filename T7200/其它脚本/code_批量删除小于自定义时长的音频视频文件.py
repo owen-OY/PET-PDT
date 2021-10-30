ef9531f105b920e178329c018436a326c829fb8b
anker kintan <88662668+ankerKintan@users.noreply.github.com>
2021-10-30 17:43:20 +0800

    Update code_批量删除小于自定义时长的音频视频文件.py

--- T7200/其它脚本/code_批量删除小于自定义时长的音频视频文件.py
+++ T7200/其它脚本/code_批量删除小于自定义时长的音频视频文件.py
@@ -2,7 +2,7 @@
 
 # @Author       : xuanye.wang
 # @Date         : 2021/10/29 22:14
-# @File         : code_批量删除小于自定义时长的音频视频文件.py
+# @File         : code_批量删除小于3S的音频视频文件.py
 '''
 @Mark:          :1、该脚本只能删除音频视频文件；
                  2、输入需要目录路径即可；
@@ -17,9 +17,14 @@ def deleteFile(path):
     for root, dirs, files in os.walk(f'{path}'):  # 判断下载的音频时长，小于3S则删除该文件
         for file in files:
             musicPath = rf'{path}{file}'
-            time.sleep(0.2)
-            mp3Info = eyed3.load(musicPath).info.time_secs
-            if mp3Info < 3: # 3S
+            # time.sleep(0.2)
+            try:
+                mp3Info = eyed3.load(musicPath).info.time_secs
+                print(musicPath)
+                if mp3Info < 3:
+                    logger.info('正在删除文件--%s' % musicPath)
+                    os.remove(musicPath)
+            except:
                 logger.info('正在删除文件--%s' % musicPath)
                 os.remove(musicPath)
 
